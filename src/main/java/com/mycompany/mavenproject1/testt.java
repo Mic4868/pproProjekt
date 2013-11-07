4989e0e9b0c8b07bcd7c4c079b8184cbd97dfc73
mira <mira@mira-U36SD>
2013-11-07 22:06:45 +0100

    Pokus o commit2

---
+++ src/main/java/com/mycompany/mavenproject1/testt.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author mira
+ */
+public class testt {
+    
+}
